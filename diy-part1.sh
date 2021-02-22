2d0a01c43b636b9ee115c43e8aee9a815163c9de
Ne <3661428@gmail.com>
2021-02-22 23:41:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,3 @@
 # Add a feed source
 #sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
 
-git clone https://github.com/fw876/helloworld.git package/luci-app-ssr-plus
